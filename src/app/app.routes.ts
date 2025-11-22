083aa2971514989b15af6ac7e28ef2db7a9338f7
Vikas <65715532+vikasd88@users.noreply.github.com>
2025-11-22 11:42:57 +0530

    free user setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,31 +3,30 @@ import { StockAlerts } from './components/stock-alerts/stock-alerts';
 import { authGuard, publicGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { 
-    path: '', 
-    redirectTo: '/alerts', 
-    pathMatch: 'full' 
+  {
+    path: '',
+    redirectTo: '/alerts',
+    pathMatch: 'full'
   },
-  { 
-    path: 'alerts', 
+  {
+    path: 'alerts',
     component: StockAlerts,
-    title: 'Stock Alerts',
-    canActivate: [authGuard]
+    title: 'Stock Alerts'
   },
-  { 
-    path: 'login', 
+  {
+    path: 'login',
     loadComponent: () => import('./components/login/login').then(m => m.LoginComponent),
     title: 'Login',
     canActivate: [publicGuard]
   },
-  { 
-    path: 'subscription', 
+  {
+    path: 'subscription',
     loadComponent: () => import('./components/subscription/subscription').then(m => m.Subscription),
     title: 'Subscription',
     canActivate: [authGuard]
   },
-  { 
-    path: '**', 
-    redirectTo: '/alerts' 
+  {
+    path: '**',
+    redirectTo: '/alerts'
   }
 ];
